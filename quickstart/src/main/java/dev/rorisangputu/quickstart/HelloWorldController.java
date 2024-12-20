8667228f4360ce09d380de64c26254b48dafbfdc
Rorisang Putu <rorisangputu@gmail.com>
2024-12-20 16:12:47 +0200

    importing get mapping annotation

--- quickstart/src/main/java/dev/rorisangputu/quickstart/HelloWorldController.java
+++ quickstart/src/main/java/dev/rorisangputu/quickstart/HelloWorldController.java
@@ -1,10 +1,12 @@
 package dev.rorisangputu.quickstart;
 
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 public class HelloWorldController {
 
+    @GetMapping(path = "/hellow")
     public String helloWorld() {
         return "Hello World";
     }
